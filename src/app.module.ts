2a9602e27fadb5819136d8b0b8ae10aed3f0243c
enojen <enesozel.eo@gmail.com>
2025-05-10 13:57:21 +0200

    reformat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
-import { CustomerController } from './customer/customer.controller';
-import { DriverController } from './driver/driver.controller';
-import { MapClient } from './common/client/map.client';
-import { CustomerModule } from './customer/customer.module';
-import { DriverModule } from './driver/driver.module';
-import { NotificationClient } from './common/client/notification.client';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [
-    ConfigModule,
-    CustomerModule,
-    DriverModule,
-    DatabaseModule,
-    HealthModule,
-  ],
-  controllers: [CustomerController, DriverController],
-  providers: [MapClient, NotificationClient],
-  exports: [MapClient],
+  imports: [ConfigModule, DatabaseModule, HealthModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
